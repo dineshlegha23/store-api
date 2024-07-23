1f7a1baf9b7ad8612d44981764e13859bcef7f5e
Dinesh <dineshlegha23@gmail.com>
2024-07-23 14:13:04 +0530

    added filtering and sorting

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,67 @@ const asyncWrapper = require("../middlewares/async");
 const Product = require("../models/productModel");
 
 const getAllProducts = asyncWrapper(async (req, res) => {
-  const products = await Product.find({});
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObj.company = company;
+  }
+
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+      ">": "$gt",
+      ">=": "$gte",
+    };
+    const regex = /\b(<|<=|=|>|>=)\b/g;
+
+    let filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorMap[match]}`
+    );
+
+    const numericOptions = ["price", "rating"];
+
+    filters.split(",").forEach((field) => {
+      const [type, operator, value] = field.split("-");
+      if (numericOptions.includes(filed)) {
+        queryObj[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  let result = Product.find(queryObj);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  const products = await result;
+
   res.status(200).json({ status: "success", total: products.length, products });
 });
 
